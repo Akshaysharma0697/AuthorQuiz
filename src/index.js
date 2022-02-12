515883d7c95d85bbff086654d60f4e4026427a24
Akshay Sharma <akshay.ai.sharma@accenture.com>
2022-02-12 15:16:36 +0530

    Files updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,115 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, withRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
+import registerServiceWorker from './registerServiceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const authors = [
+    {
+      name: 'Mark Twain',
+      imageUrl: 'images/authors/marktwain.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+      name: 'Joseph Conrad',
+      imageUrl: 'images/authors/josephconrad.png',
+      imageSource: 'Wikimedia Commons',
+      books: ['Heart of Darkness']
+    },
+    {
+      name: 'J.K. Rowling',
+      imageUrl: 'images/authors/jkrowling.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Daniel Ogren',
+      books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+      name: 'Stephen King',
+      imageUrl: 'images/authors/stephenking.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Pinguino',
+      books: ['The Shining', 'IT']
+    },
+    {
+      name: 'Charles Dickens',
+      imageUrl: 'images/authors/charlesdickens.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+      name: 'William Shakespeare',
+      imageUrl: 'images/authors/williamshakespeare.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    }
+  ];
+
+//this function is required to add all our books then shuffle our books and then provide any 4 to the screen 
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);//p is the empty list where we are concatinating c.books 
+    }, []);//this will add all the books into a list
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);//takng first four random books from the allBooks list
+    const answer = sample(fourRandomBooks);//choosing correct answer from these 4 options
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => 
+            author.books.some((title) => 
+                title === answer))//this is basically choosing an author that has written a book with the same name as the answer we selected
+    }
+}
+
+function resetState() {
+  return {
+    turnData: getTurnData(authors),
+    highlight: ''
+  };
+}
+
+let state = resetState();
+//this is the function to get the data for the game
+
+
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  state.highlight = isCorrect ? 'correct' : 'wrong';
+  render();//this tells the application needs to be rendered again
+}
+
+function App() {
+  return <AuthorQuiz {...state} 
+    onAnswerSelected={onAnswerSelected} 
+    onContinue={() => {
+      state = resetState();
+      render();//rerender application
+    }}/>;
+}
+
+const AuthorWrapper = withRouter(({ history }) =>
+  <AddAuthorForm onAddAuthor={(author) => {
+    authors.push(author);
+    history.push('/');
+  }} />
 );
+
+function render() {
+  ReactDOM.render(
+  <BrowserRouter>
+    <React.Fragment>
+      <Route exact path="/" component={App} />
+      <Route path="/add" component={AuthorWrapper} />
+    </React.Fragment>
+  </BrowserRouter>, document.getElementById('root'));
+}
+render();
+registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
